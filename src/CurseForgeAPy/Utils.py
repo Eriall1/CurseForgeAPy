e2fa0d08881816406f762d1e0e612e3b68863153
James2854 <jamesmoose2854@gmail.com>
2023-01-02 04:11:41 +0800

    name change adjustments

--- src/CurseForgeAPy/Utils.py
+++ src/CurseForgeAPy/Utils.py
@@ -25,26 +25,3 @@ def downloadFileFromModIDVersion(self: CurseForgeAPI, modID: str, version: str, 
         if version in file.gameVersions and (file.releaseType == releaseType or not releaseType):
             return downloadFileFromURL(file['downloadUrl'], filename)
     raise Exception('Version not found matching the given release type.')
-
-def create_datetime(date_string: str) -> datetime:
-    # Find the index of "T"
-    t_index = date_string.index("T")
-
-    # Slice the string into its date and time parts
-    date_part = date_string[:t_index]
-    time_part = date_string[t_index+1:]
-
-    # Split the date and time parts into their individual components
-    year, month, day = date_part.split("-")
-    hour, minute, second = time_part.split(":")
-    second = second[0:2]
-
-    # Parse the microseconds part if it is present
-    if "." in time_part:
-        dot_index = time_part.index(".")
-        microseconds_part = time_part[dot_index+1:-1]
-        date = datetime(int(year), int(month), int(day), int(hour), int(minute), int(second), int(microseconds_part))
-    else:
-        date = datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
-
-    return date
